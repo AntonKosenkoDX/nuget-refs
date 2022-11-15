1a92608142dfb51f68b7054d691e821942730cc6
Anton Kosenko <anton.kosenko@devexpress.com>
2022-11-15 15:01:49 +0400

    Init

---
+++ MyLib/Class1.cs
@@ -0,0 +1,16 @@
+﻿using MyUtilities;
+
+namespace MyLib;
+
+// All the code in this file is included in all platforms.
+public class Class1
+{
+    public void MyMethod(){
+        var test = new MyUtilities.Class1();
+        test.SomeMethod();
+    }
+}
+
+public class Class2 : MyUtilities.Class1 {
+    
+}
---
+++ MyUtilities/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace MyUtilities;
+
+// All the code in this file is included in all platforms.
+public class Class1
+{
+    public void SomeMethod(){
+        System.Console.WriteLine();
+    }
+}
